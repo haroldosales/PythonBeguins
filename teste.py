a69a13a1164193b32c98719f688ec7d547fd60a8
haroldosales <haroldognu@gmail.com>
2020-05-02 00:46:34 -0300

    Primeiro Codigo simples

---
+++ teste.py
@@ -0,0 +1,30 @@
+
+
+print("---Receita de Miojo------ ");
+
+print("---------------------------------/n");
+print("Coloque Agua para Esquenta/n");
+
+
+print("--------------------------------- /n");
+print("Coloque O miojo  na panela com agua no fogao");
+
+print("---------------------------------/n");
+print("Coloque o tempero  e mexa e esperar");
+print("---------------------------------/n");
+print("comer");
+
+	#print("Coloque O miojo  na panela com agua no fogao/n");
+
+	#print("---------------------------------/n");
+
+	#print('Coloque o tempero  e mexa e esperar /n');
+
+	#print("---------------------------------/n");
+
+	#("So comer");
+
+
+
+
+
