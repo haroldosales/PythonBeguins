2c417ecfa8b60f661a65549fe191619a5b718af9
haroldosales <haroldognu@gmail.com>
2021-02-03 20:45:36 -0300

    up study

---
+++ capituloTree.py
@@ -0,0 +1,17 @@
+def print_lyrics():
+    print("I'm a lumberjack m and I'm okay")
+    print("I sleep all night and I workd all")
+ 
+
+
+print_lyrics()
+
+print("####")
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+
+print("------######")
+
+repeat_lyrics()
---
+++ numeros.py
@@ -0,0 +1,5 @@
+print("####-#####-####")
+x =int(input("Digite Algum numero: "))
+while x >= 2:
+    x = x+1
+    pass
